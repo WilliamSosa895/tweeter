4111090fc7407ab8124f25004b6001de34120d86
WilliamSosa895 <zs23004708@estudiantes.uv.mx>
2025-05-13 09:46:10 -0600

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {FormsModule } from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TweetsComponent } from './tweets/tweets.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NewUserComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    HomeComponent,
+    LandingPageComponent,
+    TweetsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
